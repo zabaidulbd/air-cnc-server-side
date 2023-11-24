8c4636502630ecee4d1547f007ed8f70d628a089
zabaidulbd <zislam5622@gmail.com>
2023-11-24 16:34:08 +0600

    add api for saving user in db

--- index.js
+++ index.js
@@ -30,6 +30,24 @@ async function run() {
     const roomsCollection = client.db('aircncDb').collection('rooms')
     const bookingsCollection = client.db('aircncDb').collection('bookings')
 
+
+    // Save users email and role in DB
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: user,
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      console.log(result)
+      res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
